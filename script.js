6fcadd9bd4c81b7d74a6cffcd069ca6031a42501
Ahmed <ahmedlekin.97@gmail.com>
2021-06-15 22:21:12 +0300

    implement hold button function

--- script.js
+++ script.js
@@ -17,11 +17,19 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
-const score = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
-// Rolling dice functionality
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+// 1. Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   // 1. Generating random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -36,10 +44,18 @@ btnRoll.addEventListener("click", function () {
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+  // 2. Check if player's score is >= 100
+  // Finish game
+
+  // 3. Switch to next player
+  switchPlayer();
 });
